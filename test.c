6bb44c8893f6ba737d31f0172e34d7b8408c6364
jackybatsam <jackybatsam006@gmail.com>
2014-07-13 17:38:01 +0800

    add a test.c file

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	int i = 0;
+	printf("Hello World!! :D");
+	prinff("I am the real master");
+	for(i = 0; i<10 ; i++){
+		printf("%d",i);
+		if(i<5) printf("low");
+		else prinf("high");
+	}
+	printf("ififelse");
+}
